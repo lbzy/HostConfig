1a94edc2f52fb45d0818983badd97dcc6d7ac002
lbzy <1293685219@qq.com>
2019-02-13 14:51:47 +0800

    update
    更新正式版

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# HostConfig
+######如何使用
+>composer require lbzy/host-config
+```PHP
+try {
+	// 配置根域名key
+	define('ENV_TEST', 'test');
+	define('ENV_ONLINE', 'online');
+	// 配置根域名
+	define('ROOT_HOST_LIST', [
+		ENV_ONLINE 	=> ['test1.com', 'test2.com'],
+		ENV_TEST 	=> 'localhost',
+	]);
+	// 解析
+	$hostInstance 	= \Lbzy\HostConfig\Host::instance(ROOT_HOST_LIST);
+	// define('HOST', $hostInstance->getHost());
+	// define('ROOT_HOST', $hostInstance->getRootHost());
+	// define('ROOT_HOST_KEY', $hostInstance->getRootHostKey());
+	// 获得对应的根域名key
+	// 如果访问根域名为test1.com或test2.com的地址,返回 online
+	// 如果访问根域名为localhost的地址,返回 test
+	$rootHostKey = $hostInstance->getRootHostKey();
+	// 加载对应配置
+	$configFile 	= 'yourDir' . $rootHostKey . '.php';
+	if (file_exists($configFile)) {
+		// 你的代码
+		// define('ENV_CONFIG', require $configFile);
+	} else {
+		die('config file not exists.');
+	}
+} catch (\Throwable $e) {
+	die('config error.');
+}
+```
--- src/Host.php
+++ src/Host.php
@@ -3,10 +3,15 @@ namespace Lbzy\HostConfig;
 
 class Host
 {
+	# 当前实例
 	private static $instance;
+	# 当前的域名
 	private static $host;
+	# 解析得到的根域名
 	private static $rootHost;
+	# 根域名列表
 	private static $rootHostList;
+	# 解析得到根域名对应key
 	private static $rootHostKey;
 
 	private function __construct()
@@ -15,16 +20,30 @@ class Host
 		self::parseHost();
 	}
 
+	/**
+	 * 获得实例
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:50:51+0800
+	 * @param    array                    $rootHostList [根域名列表]
+	 * @return \Lbzy\HostConfig\Host
+	 */
 	static function instance(array $rootHostList = [])
 	{
 		if (! self::$instance) {
-			self::$host 		= $_SERVER['HTTP_HOST'] ?? '';
+			self::$host 		= $_SERVER['HTTP_X_REAL_HOST'] ?? ($_SERVER['HTTP_HOST'] ?? '');
 			self::$rootHostList = $rootHostList;
 			self::$instance 	= new static;
 		}
 		return self::$instance;
 	}
 
+	/**
+	 * 检测环境
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:52:03+0800
+	 * @throws  \Lbzy\HostConfig\HostException
+	 * @return
+	 */
 	private static function check()
 	{
 		if (PHP_SAPI == 'cli') {
@@ -38,15 +57,60 @@ class Host
 		}
 	}
 
-	private static function isRootHost(string $hostString)
+	
+	/**
+	 * 解析域名
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:54:25+0800
+	 * @throws  \Lbzy\HostConfig\HostException
+	 * @return 
+	 */
+	private static function parseHost()
 	{
 		
-		switch (mb_strlen(self::$host) <=> mb_strlen($hostString)) {
+		foreach (self::$rootHostList as $rootHostKey => $rootHostValue) {
+			# 字符串形式
+			if (is_string($rootHostValue)) {
+				if (self::isRootHost($rootHostValue)) {
+					self::$rootHostKey 	= $rootHostKey;
+					self::$rootHost 	= $rootHostValue;
+					break;
+				}
+			}
+			# 数组形式
+			if (is_array($rootHostValue)) {
+				foreach ($rootHostValue as $rootHostVo) {
+					if (! is_string($rootHostVo)) {
+						throw new HostException('Root host list error.');
+					}
+					if (self::isRootHost($rootHostVo)){
+						self::$rootHostKey 	= $rootHostKey;
+						self::$rootHost 	= $rootHostVo;
+						break;
+					}
+				}
+			}
+		}
+		
+		throw new HostException('Root host not found.');
+	}
+
+	/**
+	 * 检测是否是根域名
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:53:39+0800
+	 * @param    string                   $checkHost [被检测域名]
+	 * @return   boolean
+	 */
+	private static function isRootHost(string $checkHost)
+	{
+		$checkHostLength = mb_strlen($checkHost);
+		switch (mb_strlen(self::$host) <=> $checkHostLength) {
 			case 1:
-				return mb_substr(self::$host, -1 - mb_strlen($hostString), null, 'UTF-8') == ('.' . $hostString);
+				return mb_substr(self::$host, -1 - $checkHostLength, null, 'UTF-8') == ('.' . $checkHost);
 				break;
 			case 0:
-				return self::$host == $hostString;
+				return self::$host == $checkHost;
 				break;
 			case -1:
 				return false;
@@ -55,54 +119,36 @@ class Host
 				# code...
 				break;
 		}
-
 	}
 
-	private static function parseHost()
-	{
-		
-		foreach (self::$rootHostList as $rootHostKey => $rootHostValue) {
-			
-			if (is_string($rootHostValue)) {
-				
-				if (self::isRootHost($rootHostValue)) {
-					self::$rootHostKey 	= $rootHostKey;
-					self::$rootHost 	= $rootHostValue;
-					return;
-				}
-			}
-
-			if (is_array($rootHostValue)) {
-				foreach ($rootHostValue as $rootHostVo) {
-					
-					if (! is_string($rootHostVo)) {
-						throw new HostException('Root host list error.');
-					}
-
-					if (self::isRootHost($rootHostVo)){
-						self::$rootHostKey 	= $rootHostKey;
-						self::$rootHost 	= $rootHostVo;
-						return;
-					}
-				}
-			}
-
-		}
-		if (! self::$rootHost) {
-			throw new HostException('Root host not found.');
-		}
-	}
-
+	/**
+	 * 获取解析到的域名
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:55:03+0800
+	 * @return string
+	 */
 	function getHost()
 	{
 		return self::$host;
 	}
 
+	/**
+	 * 获取根域名
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:55:23+0800
+	 * @return string
+	 */
 	function getRootHost()
 	{
 		return self::$rootHost;
 	}
 
+	/**
+	 * 获取根域名对应的key
+	 * @author Lbzy
+	 * @DateTime 2019-02-13T13:56:00+0800
+	 * @return string
+	 */
 	function getRootHostKey()
 	{
 		return self::$rootHostKey;
